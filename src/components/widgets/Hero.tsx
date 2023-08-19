1b9e0f69785e919b9403bbb98f7d530fe9019fc4
Arpan Mandal <140962896+notanyarpan@users.noreply.github.com>
2023-08-19 13:45:11 +0530

    Update Hero.tsx

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -17,10 +17,7 @@ export default component$(() => {
               <div class="max-w-3xl mx-auto">
                 <p class="text-xl text-gray-600 mb-8 dark:text-slate-400">
 Welcome to a 
-<span class="font-semibold underline decoration-wavy decoration-1 decoration-secondary-600 underline-offset-2">
-World of Possibilities
-</span>{" "}
-, Welcome to {" "}
+<b>World of Possibilities</b>, Welcome to {" "}
                   <span class="font-semibold underline decoration-wavy decoration-1 decoration-secondary-600 underline-offset-2">
                     NotAnyPlace,
                   </span>{" "}

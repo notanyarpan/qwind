4b50d1a67bdd5d21f67b629ee3ea9049a1506b3c
Arpan Mandal <140962896+notanyarpan@users.noreply.github.com>
2023-09-19 08:37:47 +0200

    Update RouterHead.tsx

--- src/components/common/RouterHead.tsx
+++ src/components/common/RouterHead.tsx
@@ -14,7 +14,8 @@ export const RouterHead = component$(() => {
 
       <link rel="canonical" href={String(loc.url)} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="icon" type="image/png" href="/logo.png" />
+
 
       {head.meta.map((m, i) => (
         <meta key={i} {...m} />
